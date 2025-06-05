3ba2cc546771e53f6b5f11c7407c3b4f280c7f2b
FlamestarRS <FlamestarRS@gmail.com>
2025-06-05 02:52:25 -0400

    added explore func

--- repl.go
+++ repl.go
@@ -19,12 +19,16 @@ func startRepl() {
 		cache:    *pokecache.NewCache(5 * time.Second),
 		Next:     "https://pokeapi.co/api/v2/location-area/",
 		Previous: "",
+		Name:     "",
 	}
 
 	for {
 		fmt.Print("Pokedex > ")
 		scanner.Scan()
 		command := cleanInput(scanner.Text())
+		if len(command) > 1 {
+			config.Name = command[1]
+		}
 		output := commands(command[0], &config)
 		if output != nil {
 			err := output()
@@ -32,6 +36,7 @@ func startRepl() {
 				fmt.Println("Error:", err)
 			}
 		}
+		config.Name = ""
 	}
 }
 
@@ -74,6 +79,11 @@ func getCommands() map[string]cliCommand {
 			description: "Displays the previous 20 locations",
 			callback:    commandMapb,
 		},
+		"explore": {
+			name:        "explore",
+			description: "Displays all Pokemon in an area",
+			callback:    commandExplore,
+		},
 	}
 }
 
@@ -87,6 +97,7 @@ type config struct {
 	cache    pokecache.Cache
 	Next     string
 	Previous string
+	Name     string
 }
 
 func commandExit(config *config) error {
@@ -209,6 +220,105 @@ func commandMapb(config *config) error {
 	config.cache.Add(config.Previous, body)
 	config.Next = locations.Next
 	config.Previous = locations.Previous
+
+	return nil
+}
+
+type Explore struct {
+	ID                   int    `json:"id"`
+	Name                 string `json:"name"`
+	GameIndex            int    `json:"game_index"`
+	EncounterMethodRates []struct {
+		EncounterMethod struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"encounter_method"`
+		VersionDetails []struct {
+			Rate    int `json:"rate"`
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+		} `json:"version_details"`
+	} `json:"encounter_method_rates"`
+	Location struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"location"`
+	Names []struct {
+		Name     string `json:"name"`
+		Language struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"language"`
+	} `json:"names"`
+	PokemonEncounters []struct {
+		Pokemon struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"pokemon"`
+		VersionDetails []struct {
+			Version struct {
+				Name string `json:"name"`
+				URL  string `json:"url"`
+			} `json:"version"`
+			MaxChance        int `json:"max_chance"`
+			EncounterDetails []struct {
+				MinLevel        int   `json:"min_level"`
+				MaxLevel        int   `json:"max_level"`
+				ConditionValues []any `json:"condition_values"`
+				Chance          int   `json:"chance"`
+				Method          struct {
+					Name string `json:"name"`
+					URL  string `json:"url"`
+				} `json:"method"`
+			} `json:"encounter_details"`
+		} `json:"version_details"`
+	} `json:"pokemon_encounters"`
+}
+
+func commandExplore(config *config) error {
+	if config.Name == "" {
+		fmt.Println("Please enter the name of an area")
+	}
+	url := "https://pokeapi.co/api/v2/location-area/" + config.Name
+
+	if val, ok := config.cache.Get(config.Name); ok {
+		var explore Explore
+		err := json.Unmarshal(val, &explore)
+		if err != nil {
+			return fmt.Errorf("error unmarshaling JSON: %s", err)
+		}
+		for _, loc := range explore.PokemonEncounters {
+			fmt.Println(loc.Pokemon.Name)
+		}
+		return nil
+	}
+
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+
+	if res.StatusCode > 299 {
+		return fmt.Errorf("response failed with status code: %v", res.StatusCode)
+	}
+	if err != nil {
+		return err
+	}
+
+	var explore Explore
+	err = json.Unmarshal(body, &explore)
+	if err != nil {
+		return fmt.Errorf("error unmarshaling JSON: %s", err)
+	}
+	for _, loc := range explore.PokemonEncounters {
+		fmt.Println(loc.Pokemon.Name)
+	}
 
 	return nil
 }
